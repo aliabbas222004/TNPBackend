ccdafe06b73d833bef74298fc3603ee3c2104f95
nileshmishra280 <nileshmishra2508@gmail.com>
2025-06-24 15:17:40 +0530

    frontend link added

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000;
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:5173',
+    origin: 'https://campusconnect-tnp.onrender.com',
     methods: ['GET', 'POST']
   }
 });
@@ -61,7 +61,7 @@ io.on('connection', (socket) => {
 });
 
 // Middleware
-app.use(cors({ origin: 'http://localhost:5173' }));
+app.use(cors({ origin: 'https://campusconnect-tnp.onrender.com' }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/student", require('./routes/student'));
